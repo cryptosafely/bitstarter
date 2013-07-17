fa1c311f6a17cbe32f7c85308fd30bed8888e610
proglbyte <proglobyte@yahoo.com>
2013-07-17 22:50:28 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
-var express = require('express');
+var express = require("express");
+var app = express();
 
-var app = express.createServer(express.logger());
+var fs = require('fs');
+var str = fs.readFileSync('index.html');
+var buf = new Buffer(str.length);
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
