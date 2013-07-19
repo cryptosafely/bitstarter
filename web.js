3c2dcf6e90bc96ae6cb8e59b09366cdda9fed932
proglbyte <proglobyte@yahoo.com>
2013-07-19 07:11:54 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var sys = require("sys"),
+my_http = require("http");
 var fs  = require("fs");
-
-var line = fs.readFileSync('./index.html', "utf8").toString
-    console.log(line);
-
 var buffer = new Buffer(20);
+my_http.createServer(function(request,response){
+	sys.puts("I got kicked");
+	response.writeHeader(200, {"Content-Type": "text/plain"});
+	fs.readFileSync('./index.html', "utf8").toString;
+	response.end();
+}).listen(8080);
+sys.puts("Server Running on 8080");	
+
 
 
 
