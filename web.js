58158be55fcda7b0a8175a636cf0e21e4f6255a2
proglbyte <proglobyte@yahoo.com>
2013-07-19 07:04:21 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 var fs  = require("fs");
 
-fs.readFileSync('./index.html', "utf8").toString().split('\n').forEach(function (line) { 
+var line = fs.readFileSync('./index.html', "utf8").toString
     console.log(line);
 
 var buffer = new Buffer(20);
 
-});
 
 
 
