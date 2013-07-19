9839a6b4e97e3d37a5221112de50595dc4f22af1
proglbyte <proglobyte@yahoo.com>
2013-07-19 06:51:52 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,19 +1,13 @@
 
-var http = require('http');
 
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/html'});
+
+
 
 var fs = require('fs');
 
-var str = fs.readFileSync('index.html', "utf8", function(err, data) {
-if (err) throw err;
+var str = fs.readFileSync('index.html', "utf8");
 var buffer = new Buffer(str);
-res.write(str);
-});
+console.log(str);
 
-res.end();
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
 
 
