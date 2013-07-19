e734a3bafa01bea75f282dc59bc8832930ce2b6d
proglbyte <proglobyte@yahoo.com>
2013-07-19 07:32:21 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ var fs  = require("fs");
 var buffer = new Buffer(20);
 http.createServer(function(request,response){
 	response.writeHeader(200, {"Content-Type": "text/plain"});
-	response.write(fs.readFileSync('./index.html', "utf8"));
+	buffer = fs.readFileSync('./index.html', "utf8").toString;
+	response.write(buffer);
 	response.end();
 })
 
