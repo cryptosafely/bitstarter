ae4b0b770885e4ae7aea784d634a78c30fea3997
proglbyte <proglobyte@yahoo.com>
2013-07-19 21:43:51 +0000

    last web.js update

--- web.js
+++ web.js
@@ -1,18 +1,19 @@
-var fs = require("fs");
-var fileName = "index.html";
-fs.exists(fileName, function(exists) {
-  if (exists) {
-    fs.stat(fileName, function(error, stats) {
-      fs.open(fileName, "r", function(error, fd) {
-        var buffer = new Buffer(stats.size);
-        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
-          var data = buffer.toString("utf8", 0, buffer.length);
-          console.log(data);
-          fs.close(fd);
-        });
-      });
-    });
-  }
+var express = require('express')
+  , fs  = require('fs')
+  , app = express()
+  , port = process.env.PORT || 5000;
+
+app.use(express.logger());
+
+var data = new Buffer(fs.readFileSync('index.html'));
+
+
+app.get('/', function(req, res){
+  res.send(data.toString('utf-8'));
+});
+
+app.listen(port, function(){
+  console.log('Listenting on ' + port);
 });
 
 
