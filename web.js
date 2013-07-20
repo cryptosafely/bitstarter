fb53138d840ac095023d8747b4ed7abb7bae1ccc
proglbyte <proglobyte@yahoo.com>
2013-07-20 08:03:43 +0000

    last update of web.js

--- web.js
+++ web.js
@@ -1,6 +1,17 @@
 var fs  = require('fs');
 
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
 var data = new Buffer(fs.readFileSync('index.html'));
 
-  console.log(data.toString('utf-8'));
+app.get('/', function(request, response) {
+  response.send(data.toString('utf-8'));
+});
 
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
